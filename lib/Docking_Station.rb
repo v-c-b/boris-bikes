3e847674ad08d2f7b94429343d9d86e4d611c587
Student <student@Makerss-MacBook-Pro-2.local>
2019-08-20 17:34:37 +0100

    Separated - fixed

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -27,28 +27,3 @@ end
 
 
 end
-
-
-class Bike
-  attr_accessor :functional
-
-  def initialize
-    functional = true
-    @functional = functional
-  end
-  def working?
-    return @functional
-  end
-
-  def docking(station)
-    if station.store.length < $LIMIT then
-      station.store << self
-    else fail
-    end
-  end
-
-  def report_broken
-    @functional = false
-  end
-
-end
